5bb6f5fba5ed5aa2f9d221cb4aad2041be7b2e28
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-14 02:09:42 +0800

    heappop

--- heap2.py
+++ heap2.py
@@ -1,2 +1,15 @@
 print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
+
+from heapq import heappush, heappop
+
+fruits = []
+heappush(fruits, "orange")
+heappush(fruits, "apple")
+heappush(fruits, "banana")
+
+print(heappop(fruits))
+
+print(fruits)
+
+
