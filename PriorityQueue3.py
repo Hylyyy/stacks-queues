6cce6704bf62272fe2b2a640462af4fb451e9f7e
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-14 04:15:36 +0800

    messsages

--- PriorityQueue3.py
+++ PriorityQueue3.py
@@ -45,3 +45,13 @@ class PriorityQueue:
 
     def dequeue(self):
         return heappop(self._elements)[1]
+
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(IMPORTANT, "Windshield wipers turned on")
+messages.enqueue_with_priority(NEUTRAL, "Radio station tuned in")
+messages.enqueue_with_priority(CRITICAL, "Brake pedal depressed")
+messages.enqueue_with_priority(IMPORTANT, "Hazard lights turned on")
