55b11238196f396977f0be7966410dfe59c8c030
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-14 00:10:38 +0800

    class queue

--- queues2.py
+++ queues2.py
@@ -1,2 +1,17 @@
 print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
+
+from collections import deque
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+    
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+            
+    
