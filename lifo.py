1affcc40ac6bbff367e31f9dba374696cf1ded27
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-14 01:15:19 +0800

    importing

--- lifo.py
+++ lifo.py
@@ -1,7 +1,7 @@
 print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
 
-from queues import Stack
+from queues3 import Stack
 
 lifo = Stack("1st", "2nd", "3rd")
 for element in lifo:
