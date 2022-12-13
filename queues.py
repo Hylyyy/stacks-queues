0a6fa722c353818a54cf895dca3ea9327aaad60f
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-13 23:18:31 +0800

    content

--- queues.py
+++ queues.py
@@ -2,3 +2,14 @@ print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
 
 from collections import deque
+
+class Queue:
+    def __init__(self):
+        self._elements = deque()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
+        
