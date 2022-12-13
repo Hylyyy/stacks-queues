e429990d4a2bef78653339c995504edbd237fda1
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-14 01:21:28 +0800

    appending lifo

--- lifo2.py
+++ lifo2.py
@@ -1,2 +1,12 @@
 print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
+
+lifo = []
+
+lifo.append("1st")
+lifo.append("2nd")
+lifo.append("3rd")
+
+print(lifo.pop())
+print(lifo.pop())
+print(lifo.pop())
