77ab849ec78fd15aef85bbb4c3c0cff5a9e60a19
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-14 01:39:30 +0800

    heappush

--- heap.py
+++ heap.py
@@ -1,2 +1,11 @@
 print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
+
+from heapq import heappush
+
+fruits = []
+heappush(fruits, "orange")
+heappush(fruits, "apple")
+heappush(fruits, "banana")
+
+fruits
