6052b46b4493031f00002b86d80a154cee678249
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-14 00:40:27 +0800

    print

--- queues3.py
+++ queues3.py
@@ -1,4 +1,12 @@
 print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
 
-from queues import Queue
+from queues2 import Queue
+
+fifo = Queue("1st", "2nd", "3rd")
+print(len(fifo))
+
+for element in fifo:
+    print(element)
+
+len(fifo)
