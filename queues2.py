c2d470d0261d5444c843ab13e208c724794fbb1b
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-14 00:15:08 +0800

    def enqueue and dequeue

--- queues2.py
+++ queues2.py
@@ -13,5 +13,12 @@ class Queue:
     def __iter__(self):
         while len(self) > 0:
             yield self.dequeue()
-            
+    
+    def enqueue(self, element):
+        self._elements.append(element)
+    
+    def dequeue(self):
+        return self._elements.popleft()
+        
+
     
