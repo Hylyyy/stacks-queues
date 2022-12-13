c49cb256496f24237103123c4e7fd10ffed877fa
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-14 03:14:58 +0800

    import PriorityQueue

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -1,2 +1,8 @@
 print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
+
+from priority_queue import PriorityQueue
+
+CRITICAL = 3
+IMPORTANT = 2
+NEUTRAL = 1
