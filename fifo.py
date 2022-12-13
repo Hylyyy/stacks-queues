1394c9b2a4e2ea5d617dd1364a93492d4c43b3df
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-13 23:43:34 +0800

    import Queue

--- fifo.py
+++ fifo.py
@@ -1,2 +1,9 @@
 print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
+
+from queues import Queue
+
+fifo = Queue()
+fifo.enqueue("1st")
+fifo.enqueue("2nd")
+fifo.enqueue("3rd")
