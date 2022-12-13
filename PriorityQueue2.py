9d38abc2c245b855df394c6f9786bff83cf6419d
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-14 03:40:05 +0800

    class PriorityQueue

--- PriorityQueue2.py
+++ PriorityQueue2.py
@@ -34,3 +34,13 @@ class PriorityQueue:
 
     def dequeue(self):
         return heappop(self._elements)
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (-priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)[1]
