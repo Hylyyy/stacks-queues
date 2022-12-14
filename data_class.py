22a589ddd5f1b623522b65a230da47d57b24d4c9
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-14 15:41:13 +0800

    class message

--- data_class.py
+++ data_class.py
@@ -2,3 +2,7 @@ print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
 
 from dataclasses import dataclass
+
+@dataclass
+class Message:
+    event: str
