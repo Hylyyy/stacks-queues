d12dcfcd2247202d3d3365a660044e1b994e6b68
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-14 15:43:39 +0800

    message

--- data_class.py
+++ data_class.py
@@ -6,3 +6,8 @@ from dataclasses import dataclass
 @dataclass
 class Message:
     event: str
+
+wipers = Message("Windshield wipers turned on")
+hazard_lights = Message("Hazard lights turned on")
+
+wipers < hazard_lights
