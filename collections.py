7fa4708fd0e7cd38447fa5d9355771e15b1e79db
Hydee Lyn C. Palisoc <116437907+Hylyyy@users.noreply.github.com>
2022-12-14 21:41:31 +0800

    Delete collections.py

--- collections.py
+++
@@ -1,6 +0,0 @@
-print("************  PROGRAMMED BY  ************")
-print("********* HYDEE LYN C. PALISOC *********\n")
-
-from collections import deque
-from heapq import heappop, heappush
-from itertools import count
