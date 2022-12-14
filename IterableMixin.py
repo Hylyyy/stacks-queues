3fc17de59bbabf6431b9dc113e81e30b07401454
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-15 02:15:41 +0800

    class IterableMixin

--- IterableMixin.py
+++ IterableMixin.py
@@ -4,3 +4,11 @@ print("********* HYDEE LYN C. PALISOC *********\n")
 from collections import deque
 from heapq import heappop, heappush
 from itertools import count
+
+class IterableMixin:
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
