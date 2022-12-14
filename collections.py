b1312c6ae614b90e7697501d5514259464c82b2b
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-14 17:42:12 +0800

    importing

--- collections.py
+++ collections.py
@@ -1,2 +1,6 @@
 print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
+
+from collections import deque
+from heapq import heappop, heappush
+from itertools import count
