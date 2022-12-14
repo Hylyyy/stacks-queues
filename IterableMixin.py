1ad103cb419fefd7de85d8f8dc10b1ac38d3edea
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-15 02:23:45 +0800

    def enqueue and dequeue

--- IterableMixin.py
+++ IterableMixin.py
@@ -32,3 +32,9 @@ class PriorityQueue(IterableMixin):
         self._elements = []
         self._counter = count()
 
+    def enqueue_with_priority(self, priority, value):
+            element = (-priority, next(self._counter), value)
+            heappush(self._elements, element)
+
+    def dequeue(self):
+        return heappop(self._elements)[-1]
