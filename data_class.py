e27e72f4fb2e9061d23aa3c31d880d4e213044dd
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-14 17:05:06 +0800

    class queue

--- data_class.py
+++ data_class.py
@@ -11,3 +11,24 @@ wipers = Message("Windshield wipers turned on")
 hazard_lights = Message("Hazard lights turned on")
 
 print(wipers < hazard_lights)
+
+from priority_queue import PriorityQueue
+from collections import deque
+from heapq import heappop, heappush
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+    
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+    
+    def enqueue(self, element):
+        self._elements.append(element)
+    
+    def dequeue(self):
+        return self._elements.popleft()
