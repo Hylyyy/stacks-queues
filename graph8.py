7a3498a25cd20e5281ed3ff51881ac0886c9d3f8
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-16 02:14:16 +0800

    def sort by

--- graph8.py
+++ graph8.py
@@ -6,3 +6,6 @@ from graph4 import City, load_graph
 
 graph = nx.nx_agraph.read_dot("roadmap.dot")
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+def sort_by(neighbors, strategy):
+    return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
