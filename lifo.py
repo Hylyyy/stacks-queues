32f3fedd497f1f19bee828b51902a0f439e7c6f9
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-15 18:36:19 +0800

    printing the statements

--- lifo.py
+++ lifo.py
@@ -4,5 +4,9 @@ print("********* HYDEE LYN C. PALISOC *********\n")
 from queues3 import Stack
 
 lifo = Stack("1st", "2nd", "3rd")
+print("Number of stack:", len(lifo))
+
 for element in lifo:
     print(element)
+
+print("The current number of stack:", len(lifo))
