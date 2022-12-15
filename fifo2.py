b0db2ec4e915d04d7e49c7c729442e5cd71244f7
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-15 18:23:37 +0800

    printing in queue

--- fifo2.py
+++ fifo2.py
@@ -4,7 +4,7 @@ print("********* HYDEE LYN C. PALISOC *********\n")
 from queues2 import Queue
 
 fifo = Queue("1st", "2nd", "3rd")
-print(len(fifo))
+print("In queue:", len(fifo))
 
 for element in fifo:
     print(element)
