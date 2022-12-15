1c17d93c1ad4926ba5d6dac92785185727d8a7b7
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-16 01:23:24 +0800

    class city

--- graph3.py
+++ graph3.py
@@ -2,3 +2,10 @@ print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
 
 from typing import NamedTuple
+
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
