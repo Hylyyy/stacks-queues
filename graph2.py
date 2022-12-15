6ef9be6b42db5233d4cbd451feb8d6760759b0c6
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-15 23:57:35 +0800

    graph variable

--- graph2.py
+++ graph2.py
@@ -2,3 +2,5 @@ print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
 
 import networkx as nx
+graph = nx.nx_agraph.read_dot("roadmap.dot")
+graph.nodes["london"]
