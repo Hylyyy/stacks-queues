986294ad24613736c574aafef80ae5653e0f21d8
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-16 01:41:47 +0800

    class city

--- graph4.py
+++ graph4.py
@@ -3,3 +3,10 @@ print("********* HYDEE LYN C. PALISOC *********\n")
 
 from typing import NamedTuple
 import networkx as nx
+
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int
+    latitude: float
+    longitude: float
