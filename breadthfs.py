f6f6328c85b230f2ba80d1133e13f0f6974aa41d
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-16 03:37:59 +0800

    def year

--- breadthfs.py
+++ breadthfs.py
@@ -3,3 +3,6 @@ print("********* HYDEE LYN C. PALISOC *********\n")
 
 import networkx as nx
 from graph import City, load_graph
+
+def is_twentieth_century(year):
+    return year and 1901 <= year <= 2000
