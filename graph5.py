f8e98db063deddb56451d7684fc42f31cccb792f
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-16 01:54:13 +0800

    varibale and print statement

--- graph5.py
+++ graph5.py
@@ -2,3 +2,16 @@ print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
 
 from graph import City, load_graph
+
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+print(nodes["london"])
+
+
+
+
+
+
+
+
+print(graph)
