6fc7d55158e8e2c2fbdd548bb6edf83115087d7a
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-16 16:16:27 +0800

    graph2 updated

--- graph2.py
+++ graph2.py
@@ -1,6 +1,7 @@
+import networkx as nx
 print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
 
-import networkx as nx
+
 graph = nx.nx_agraph.read_dot("roadmap.dot")
-graph.nodes["london"]
+print(graph.nodes["london"])
