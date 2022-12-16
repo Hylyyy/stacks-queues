8b52a2a86a5c080ec99c1a92cb6ecfab2b8f106e
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-16 16:29:15 +0800

    updated breadthfs

--- breadthfs.py
+++ breadthfs.py
@@ -1,11 +1,10 @@
-print("************  PROGRAMMED BY  ************")
-print("********* HYDEE LYN C. PALISOC *********\n")
-
 import networkx as nx
-from graph import City, load_graph
+from graph4 import City, load_graph
+
 
 def is_twentieth_century(year):
     return year and 1901 <= year <= 2000
+
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 for node in nx.bfs_tree(graph, nodes["edinburgh"]):
@@ -15,3 +14,4 @@ for node in nx.bfs_tree(graph, nodes["edinburgh"]):
         break
 else:
     print("Not found")
+    
