558c4cb9e926c91a104c6f752afab735056730f6
Hylyyy <palisochydeelyn97@gmail.com>
2022-12-16 16:23:28 +0800

    updated graph5

--- graph5.py
+++ graph5.py
@@ -1,17 +1,7 @@
-print("************  PROGRAMMED BY  ************")
-print("********* HYDEE LYN C. PALISOC *********\n")
-
-from graph import City, load_graph
+from graph4 import City, load_graph
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
 print(nodes["london"])
-
-
-
-
-
-
-
 
 print(graph)
